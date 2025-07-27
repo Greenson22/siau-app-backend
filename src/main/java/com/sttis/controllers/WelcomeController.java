b426654b70fe0ed21c598769ce514a5a6f4bbf2b
frendygerung22022002 <frendygerung22022002@gmail.com>
2025-07-27 10:47:15 +0000

    refactor(main): menghapus demo aplication dan sedikit perubahan strukutr

--- src/main/java/com/sttis/controllers/WelcomeController.java
+++ src/main/java/com/sttis/controllers/WelcomeController.java
@@ -1,4 +1,4 @@
-package com.example.controllers;
+package com.sttis.controllers;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
