54a84e46669e92a029478aad44db872bcd7842e3
frendygerung22022002 <frendygerung22022002@gmail.com>
2025-08-04 08:34:19 +0000

    fix: Atasi warning serialisasi dengan mengaktifkan PageSerializationMode
    Mengaktifkan @EnableSpringDataWebSupport dengan mode VIA_DTO
    di class MainApplication. Tujuannya untuk menghilangkan pesan
    peringatan dan memastikan struktur JSON dari paginasi tetap
    stabil di masa depan.

--- src/main/java/com/sttis/MainApplication.java
+++ src/main/java/com/sttis/MainApplication.java
@@ -2,11 +2,13 @@ package com.sttis;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.web.config.EnableSpringDataWebSupport; // <-- IMPORT INI
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@EnableSpringDataWebSupport(pageSerializationMode = EnableSpringDataWebSupport.PageSerializationMode.VIA_DTO) // <-- TAMBAHKAN ANOTASI INI
 public class MainApplication {
 
   @Value("${NAME:World}")
@@ -23,5 +25,4 @@ public class MainApplication {
   public static void main(String[] args) {
     SpringApplication.run(MainApplication.class, args);
   }
-
 }
